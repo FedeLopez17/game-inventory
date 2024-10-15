c47793a99aef09bb808bd87d17322da1c1254753
FedeLopez17 <fedelopez1712@gmail.com>
2024-10-15 17:12:27 -0300

    Feat - Add table creation script

---
+++ db/scripts/create-tables.sql
@@ -0,0 +1,83 @@
+CREATE TABLE pegi_ratings (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    rating VARCHAR(10) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE esrb_ratings (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    rating VARCHAR(10) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE videogames (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    release_date DATE NOT NULL,
+    rating DECIMAL(5, 2) NOT NULL CHECK (rating >= 0 AND rating <= 100),
+    cover_image_url VARCHAR(2048) NOT NULL,
+    banner_image_url VARCHAR(2048),
+    description TEXT NOT NULL,
+    website VARCHAR(2048) NOT NULL,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    pegi_rating_id INT REFERENCES pegi_ratings(id),
+    esrb_rating_id INT REFERENCES esrb_ratings(id),
+    metacritic_rating DECIMAL(5, 2) CHECK (metacritic_rating >= 0 AND metacritic_rating <= 100),
+    opencritic_rating DECIMAL(5, 2) CHECK (opencritic_rating >= 0 AND opencritic_rating <= 100),
+    ign_rating DECIMAL(5, 2) CHECK (ign_rating >= 0 AND ign_rating <= 10),
+    metacritic_review_count INT,
+    opencritic_review_count INT,
+    ign_review_count INT
+);
+
+CREATE TABLE genres (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    icon_url VARCHAR(2048)
+);
+
+CREATE TABLE videogames_genres (
+    videogame_id INT REFERENCES videogames(id),
+    genre_id INT REFERENCES genres(id),
+    PRIMARY KEY (videogame_id, genre_id)
+);
+
+CREATE TABLE publishers (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    logo_image_url VARCHAR(2048)
+);
+
+CREATE TABLE videogames_publishers (
+    videogame_id INT REFERENCES videogames(id),
+    publisher_id INT REFERENCES publishers(id),
+    PRIMARY KEY (videogame_id, publisher_id)
+);
+
+CREATE TABLE studios (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    logo_image_url VARCHAR(2048)
+);
+
+CREATE TABLE videogames_studios (
+    videogame_id INT REFERENCES videogames(id),
+    studio_id INT REFERENCES studios(id),
+    PRIMARY KEY (videogame_id, studio_id)
+);
+
+CREATE TABLE platforms (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    logo_image_url VARCHAR(2048)
+);
+
+CREATE TABLE videogames_platforms (
+    videogame_id INT REFERENCES videogames(id),
+    platform_id INT REFERENCES platforms(id),
+    PRIMARY KEY (videogame_id, platform_id)
+);
