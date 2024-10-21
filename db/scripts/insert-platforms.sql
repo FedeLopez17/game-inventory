84535dcc58cf79ac0cce38eb998238f8425bc220
FedeLopez17 <fedelopez1712@gmail.com>
2024-10-21 16:39:30 -0300

    Feat - Add platfrom images and insertion SQL script

---
+++ db/scripts/insert-platforms.sql
@@ -0,0 +1,14 @@
+INSERT INTO platforms (name, description, logo_image_url) VALUES
+('Microsoft Xbox', 'The original Xbox console that launched Microsoft into the gaming industry.', 'microsoft-xbox.png'),
+('Microsoft Xbox 360', 'A seventh-generation home video game console known for its online gaming service, Xbox Live, and a diverse library of games.', 'microsoft-xbox-360.png'),
+('Microsoft Xbox One', 'The third console in the Xbox series, known for its multimedia capabilities.', 'microsoft-xbox-one.png'),
+('Microsoft Xbox Series', 'The latest generation of Xbox consoles offering advanced graphics and performance.', 'microsoft-xbox-series.png'),
+('Nintendo Switch', 'A hybrid video game console developed by Nintendo, allowing both handheld and docked play.', 'nintendo-switch.png'),
+('Nintendo Wii U', 'A home console that features a unique gamepad with a touchscreen.', 'nintendo-wii-u.png'),
+('Nintendo Wii', 'A revolutionary console that popularized motion control gaming.', 'nintendo-wii.png'),
+('Sony Playstation', 'The original PlayStation console that introduced 3D gaming to a wider audience.', 'sony-playstation.png'),
+('Sony Playstation 2', 'The best-selling video game console of all time, known for its vast library of games.', 'sony-playstation-2.png'),
+('Sony Playstation 3', 'A powerful gaming console known for its Blu-ray support and online capabilities.', 'sony-playstation-3.png'),
+('Sony Playstation 4', 'A widely popular gaming console with a large user base and diverse game library.', 'sony-playstation-4.png'),
+('Sony Playstation 5', 'The latest generation of PlayStation consoles, offering enhanced performance and graphics.', 'sony-playstation-5.png'),
+('Windows', 'A personal computer operating system developed by Microsoft, widely used for gaming.', 'windows.png');
