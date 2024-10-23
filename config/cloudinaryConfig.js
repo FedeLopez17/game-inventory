0c961df05ea60973b0aa413c4565ce04eaa9e813
FedeLopez17 <fedelopez1712@gmail.com>
2024-10-23 17:20:33 -0300

    Feat - Add configurations for Cloudinary and Multer

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,30 @@
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const streamUpload = (fileBuffer, folderName = "game-inventory") => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: folderName },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+
+    streamifier.createReadStream(fileBuffer).pipe(stream);
+  });
+};
+
+module.exports = streamUpload;
