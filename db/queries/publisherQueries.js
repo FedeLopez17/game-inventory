79fd5f185a1593b6aa94322db287dc6f9480ae0c
FedeLopez17 <fedelopez1712@gmail.com>
2024-11-09 21:55:59 -0300

    Implement queries for publishers and studios

--- db/queries/publisherQueries.js
+++ db/queries/publisherQueries.js
@@ -5,4 +5,40 @@ module.exports = {
     const { rows } = await pool.query("SELECT * FROM publishers");
     return rows;
   },
+
+  getPublisherById: async (id) => {
+    const { rows } = await pool.query(
+      "SELECT * FROM publishers WHERE id = $1",
+      [id]
+    );
+    return rows[0];
+  },
+
+  getPublisherByName: async (name) => {
+    const { rows } = await pool.query(
+      "SELECT * FROM publishers WHERE name ILIKE $1",
+      [name]
+    );
+    return rows[0];
+  },
+
+  addPublisher: async (name, description, image) => {
+    const result = await pool.query(
+      "INSERT INTO publishers (name, description, logo_image_url) VALUES ($1, $2, $3) RETURNING *",
+      [name, description, image]
+    );
+
+    return result.rows[0];
+  },
+
+  deletePublisherById: async (id) => {
+    await pool.query("DELETE FROM publishers WHERE id = $1", [id]);
+  },
+
+  updatePublisher: async (id, name, description, imageUrl) => {
+    await pool.query(
+      "UPDATE publishers SET name = $1, description = $2, logo_image_url = $3 WHERE id = $4",
+      [name, description, imageUrl, id]
+    );
+  },
 };
--- db/queries/studioQueries.js
+++ db/queries/studioQueries.js
@@ -5,4 +5,39 @@ module.exports = {
     const { rows } = await pool.query("SELECT * FROM studios");
     return rows;
   },
+
+  getStudioById: async (id) => {
+    const { rows } = await pool.query("SELECT * FROM studios WHERE id = $1", [
+      id,
+    ]);
+    return rows[0];
+  },
+
+  getStudioByName: async (name) => {
+    const { rows } = await pool.query(
+      "SELECT * FROM studios WHERE name ILIKE $1",
+      [name]
+    );
+    return rows[0];
+  },
+
+  addStudio: async (name, description, image) => {
+    const result = await pool.query(
+      "INSERT INTO studios (name, description, logo_image_url) VALUES ($1, $2, $3) RETURNING *",
+      [name, description, image]
+    );
+
+    return result.rows[0];
+  },
+
+  deleteStudioById: async (id) => {
+    await pool.query("DELETE FROM studios WHERE id = $1", [id]);
+  },
+
+  updateStudio: async (id, name, description, imageUrl) => {
+    await pool.query(
+      "UPDATE studios SET name = $1, description = $2, logo_image_url = $3 WHERE id = $4",
+      [name, description, imageUrl, id]
+    );
+  },
 };
